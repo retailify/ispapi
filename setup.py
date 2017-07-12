b535967a1daecd643ab2cea9af519a5ff548a257
Thomas Meitz <thomas.meitz@retailify.de>
2017-07-12 10:34:14 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+]
+
+INSTALL_REQUIREMENTS = [
+    'setuptools',
+]
+
+
+setup(
+    author='Thomas Meitz',
+    author_email='info@retailify.de',
+    name='ispapi',
+    version="0.0.1",
+    description='Hexonet ISP API',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    url='https://retailify.de/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIREMENTS,
+    packages=find_packages(exclude=['project', 'project.*']),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='runtests.main',
+)
